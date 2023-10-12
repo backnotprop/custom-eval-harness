2de187e96768aa291972cc265045147c918c7f36
Rricha <rjalota@apptek.com>
2023-10-12 15:34:48 +0200

    Delete lm-evaluation-harness sub-directory

--- lm-evaluation-harness/lm_eval/tasks/climabench/cdp_topic/utils.py
+++
@@ -1,6 +0,0 @@
-import datasets
-
-
-def select_first_1100(dataset: datasets.Dataset) -> datasets.Dataset:
-    # Only supports and refutes
-    return dataset.shuffle(42).select(range(1100))
--- lm-evaluation-harness/lm_eval/tasks/climabench/clima_text/utils.py
+++
@@ -1,6 +0,0 @@
-import datasets
-
-
-def filter_binary(dataset: datasets.Dataset) -> datasets.Dataset:
-    # Only supports and refutes
-    return dataset.filter(lambda example: example["label"] in [0,1])
